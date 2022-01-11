60a23173d67c127cf19b1ae70170e4f3ff2b1c8d
dcox45 <dylanwjc@gmail.com>
2022-01-11 12:58:36 -0500

    adds slime food

--- src/main/java/com/dcox/modwroman/core/init/ItemInit.java
+++ src/main/java/com/dcox/modwroman/core/init/ItemInit.java
@@ -14,9 +14,13 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.TallBlockItem;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.item.Item.Properties;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -25,8 +29,11 @@ public class ItemInit {
 	
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModwRoman.MOD_ID);
     
-    public static final RegistryObject<ExampleItem> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new ExampleItem(new Item.Properties().tab(ItemGroup.TAB_MISC)));
-    public static final RegistryObject<FlameThrower> FLAME_THROWER = ITEMS.register("flame_thrower", () -> new FlameThrower(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<ExampleItem> EXAMPLE_ITEM = ITEMS.register("example_item", 
+    		() -> new ExampleItem(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    
+    public static final RegistryObject<FlameThrower> FLAME_THROWER = ITEMS.register("flame_thrower",
+    		() -> new FlameThrower(new Item.Properties().tab(ItemGroup.TAB_MISC)));
     
     public static final RegistryObject<BlockItem> MYSLIMEBLOCK = ITEMS.register("myslime_block", 
 			() -> new BlockItem(BlockInit.MYSLIME_BLOCK.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
@@ -34,19 +41,17 @@ public class ItemInit {
     
     public static final RegistryObject<TallBlockItem> DYLANS_DOOR = ITEMS.register("dylans_door", 
 			() -> new TallBlockItem(BlockInit.DYLANS_DOOR.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
-    
-    
-//    public static final Item ACACIA_DOOR = registerBlock(new TallBlockItem(Blocks.ACACIA_DOOR,
-//    		(new Item.Properties()).tab(ItemGroup.TAB_REDSTONE)));
-
-    
-    
+       
 	
 	public static final RegistryObject<SpecialItem> SPECIAL_ITEM = ITEMS.register("special_item", 
 			() -> new SpecialItem(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 	
-    public static final RegistryObject<LightningWand> lightning_wand = ITEMS.register("lightning_wand", () -> new LightningWand(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<LightningWand> lightning_wand = ITEMS.register("lightning_wand",
+    		() -> new LightningWand(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
-			
+	public static final RegistryObject<Item> my_food = ITEMS.register("my_food",
+			() -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)
+					.food(new Food.Builder().effect(() -> new EffectInstance(Effects.JUMP, 1000, 2), 1.0f)
+							.fast().nutrition(1).saturationMod(0.1f).alwaysEat().build())));		
 
 }
